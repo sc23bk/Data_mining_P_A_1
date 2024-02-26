408ecbb23945dd5efcf50f6c576a0391a639468a
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-02-26 18:20:04 -0500

    Update new_utils.py

--- new_utils.py
+++ new_utils.py
@@ -10,22 +10,3 @@
   
      import new_utils as nu
 """
-
-import numpy as np
-from numpy.typing import NDArray  
-
-#1-B
-def scale_data(X_bi = NDArray[np.floating]):
-    # Check if all elements are floating-point numbers and within the range [0, 1]
-    if not issubclass(X_bi.dtype.type, np.floating) or (X_bi < 0).any() or (X_bi > 1).any():
-        return False
-
-    return True
-
-#1-B
-def scale_data_1(y_bi = NDArray[np.int32]):
-    # Check if the elements in y are integers or not
-    if not issubclass(y_bi.dtype.type, np.int32):
-        return False
-    
-    return True
